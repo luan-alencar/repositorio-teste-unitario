66586852981720ce2d54e5536b6d21148de911b8
Luan-Augusto <luanalencar134@gmail.com>
2020-09-30 11:11:55 -0300

    add comentarios em testEmpresa

--- engenharia-teste-unitario/src/test/java/david/augusto/luan/testEmpresa.java
+++ engenharia-teste-unitario/src/test/java/david/augusto/luan/testEmpresa.java
@@ -5,17 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Test;
 
 public class testEmpresa {
-
+    // inicia dando um nome ao obj "e" que instancia Empresa
     Empresa e = new Empresa("BASIS");
 
     @Test
     public void testGetNome() {
+        // testa se o nome do obj "e" localizado na memoria e igual a sequencia
+        // de caracteres "BASIS"
         assertEquals("BASIS", e.getNome());
     }
 
     @Test
     public void testSetNome() {
+        // modificando o nome para "Accenture"
         e.setNome("Accenture");
+        // verificando se o obj "e" e igual a sequencia de caracteres "Accenture"
         assertEquals("Accenture", e.getNome());
     }
 
